f165c862d5600b8552d913c6f59c2f104de8d8d4
Your Name <you@example.com>
2023-02-14 16:47:03 -0500

    Created variable for # of rounds, for loop

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,21 @@
+import java.util.Scanner
+
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    static String[] responses = 
+
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    String rounds = myObj.nextLine(); //reads user's input, stores in 'rounds' variable
+    
+    for (int i = 0; i < rounds; i++){
+      System.out.println("Hi. Give me your name now.");
+      String userName = myObj.nextLine();
+      System.out.println("What's on your mind today", userName, "?");
+      String s1 = myObj.nextLine();
+    }
+    
   }
 }
