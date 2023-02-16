fcdcadee0676096a70cbcc14f5827ca21a9640f1
Your Name <you@example.com>
2023-02-15 20:30:58 -0500

    Random responses implemented

--- Conversation.java
+++ Conversation.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-import java.lang.StringBuilder;
+import java.util.ArrayList;
+import java.util.Random;
 
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
-    // static String[] reply = {"Shocking", "I'm shocked.", "Are you okay?", "Uh huh.", "Wow.", "Hmmm."};
+    String[] replyStrings = {"Shocking", "I'm shocked.", "Are you okay?", "Uh huh.", "Wow.", "Hmmm."};
     // static String[] transcript = 
 
     Scanner myObj = new Scanner(System.in);
@@ -17,6 +18,7 @@ class Conversation {
     for (int i = 0; i < rounds; i++){
       System.out.println("What's on your mind right now " + userName + "?");
       String s1 = myObj.nextLine();
+      
       // I -> you
       // me -> you
       // am -> are
@@ -25,39 +27,52 @@ class Conversation {
       // your -> my  
       
       String[] words = s1.split(" ");
-      for (int w = 0; w < words.length; w++){
-        if (words[w] == "I"){
-          words[w].replace("I", "you");
-          
-        }
-        if (words[w] == "me"){
-          words[w].replace("me", "you");
-          
-        }
-        if (words[w] == "am"){
-          words[w].replace("am", "are");
-          
-        }
-        if (words[w] == "you"){
-          words[w].replace("you", "I");
-          
-        }
-        if (words[w] == "my"){
-          words[w].replace("my", "your");
-          
-        }
-        if (words[w] == "your"){
-          words[w].replace("your", "my");
-          
-        }
-        else{
-          
-        }
-      }
-      System.out.println();
+      ArrayList newSentence = new ArrayList <>();
+      Random random = new Random();
+      int index = random.nextInt(replyStrings.length);
+      System.out.println(replyStrings[index]);
       
-    }
+      if (newSentence.contains("I")){
+        
+      }
+
+
+      // for (int w = 0; w < words.length; w++){
+      //   if (words[w] != "I" && words[w] != "me" && words[w] != "am" && words[w] != "you" && words[w] != "my" && words[w] != "your"){
+      //     String word = words[w];
+      //     newSentence.add(words[w]);
+      //   }
+      //   else if (words[w] == "me"){
+      //     String word = words[w].replace("me", "you");
+      //     newSentence.add(word);
+      //   }
+      //   else if (words[w] == "am"){
+      //     String word = words[w].replace("am", "are");
+      //     newSentence.add(word);
+      //   }
+      //   else if (words[w] == "you"){
+      //     String word = words[w].replace("you", "I");
+      //     newSentence.add(word);
+      //   }
+      //   else if (words[w] == "my"){
+      //     String word = words[w].replace("my", "your");
+      //     newSentence.add(word);
+      //   }
+      //   else if (words[w] == "your"){
+      //     String word = words[w].replace("your", "my");
+      //     newSentence.add(word);
+      //   }
+      //   else if (words[w] == "I"){
+      //     String word = words[w].replace("I", "you");
+      //     newSentence.add(word);
+    
+      // }
+      // String responseString = String.join(" ", newSentence);
+      // System.out.println(responseString);
+      
+    // }
     System.out.println("Goodbye!");
     
   }
+}
 }
